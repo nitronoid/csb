4f4b0f9331e2c86450d49804528aaab1857346b9
nitronoid <jackdiver@hotmail.co.uk>
2018-02-21 16:42:08 +0000

    Created a map for shader reuse within the shader lib class

--- include/ShaderLib.h
+++ include/ShaderLib.h
@@ -19,10 +19,10 @@ public:
   //-----------------------------------------------------------------------------------------------------
   /// @brief Creates a shader program and loads a vertex and fragment shader, attaching both.
   /// @param [in] _name is the name that this shader program should be stored under.
-  /// @param [in] _vertexName is the file path to the vertex glsl shader.
-  /// @param [in] _fragName is the file path to the fragment glsl shader.
+  /// @param [in] _shaderPaths contains paths to the vertex, fragment, and geometry shaders in that order,
+  /// any paths left blank are ignored.
   //-----------------------------------------------------------------------------------------------------
-  void createShader(const std::string& _name, const QString &_vertexName, const QString &_fragName, const QString& _geoName = "");
+  void createShader(const std::string &_name, const std::array<QString, 3> &_shaderPaths);
   //-----------------------------------------------------------------------------------------------------
   /// @brief Binds a stored shader.
   /// @param [in] _name is the name of the shader program that should be bound.
@@ -41,10 +41,15 @@ public:
   QOpenGLShaderProgram* getCurrentShader();
 
 private:
+  enum SHADER_TYPES {VERTEX, FRAGMENT, GEOMETRY};
   //-----------------------------------------------------------------------------------------------------
   /// @brief A map from shader name to shader program, so that they can be reused and easily bound.
   //-----------------------------------------------------------------------------------------------------
   std::unordered_map<std::string, std::unique_ptr<QOpenGLShaderProgram>> m_shaderPrograms;
+  //-----------------------------------------------------------------------------------------------------
+  /// @brief A map from shader name to shader, so that they can be reused by shader programs.
+  //-----------------------------------------------------------------------------------------------------
+  std::unordered_map<std::string, std::unique_ptr<QOpenGLShader>> m_shaderParts;
   //-----------------------------------------------------------------------------------------------------
   /// @brief A pointer to the currently bound shader program.
   //-----------------------------------------------------------------------------------------------------
--- src/ShaderLib.cpp
+++ src/ShaderLib.cpp
@@ -5,6 +5,7 @@
 
 std::string ShaderLib::loadShaderProg(const QString &_jsonFileName)
 {
+  auto toStr = [](const auto& val){ return val.toString(); };
   // Read in raw file
   QFile file(_jsonFileName);
   file.open(QIODevice::ReadOnly | QIODevice::Text);
@@ -17,27 +18,43 @@ std::string ShaderLib::loadShaderProg(const QString &_jsonFileName)
   // Get a string out from the json
   std::string shaderName = shaderParts["Name"].toString().toStdString();
 
-  QString geoName = "";
-  if (shaderParts.contains("Geometry"))
-    geoName = shaderParts["Geometry"].toString();
+  enum {VERTEX, FRAGMENT, GEOMETRY};
+  constexpr std::array<const char*, 3> shaderNames = {{"Vertex", "Fragment", "Geometry"}};
+  std::array<QString, 3> shaderPaths;
+
+  for (auto shader : {VERTEX, FRAGMENT, GEOMETRY})
+  {
+    auto& name = shaderNames[shader];
+    shaderPaths[shader] = shaderParts.contains(name) ? toStr(shaderParts[name]) : "";
+  }
 
   // Load the shader if we haven't already
   if (!m_shaderPrograms.count(shaderName))
-    createShader(shaderName, shaderParts["Vertex"].toString(), shaderParts["Fragment"].toString(), geoName);
+    createShader(shaderName, shaderPaths);
 
   return shaderName;
 }
 
-void ShaderLib::createShader(const std::string &_name, const QString &_vertexName, const QString &_fragName, const QString &_geoName)
+void ShaderLib::createShader(const std::string &_name, const std::array<QString, 3> &_shaderPaths)
 {
   QOpenGLShaderProgram *program = new QOpenGLShaderProgram();
 
-  program->addShaderFromSourceFile(QOpenGLShader::Vertex, _vertexName);
-  program->addShaderFromSourceFile(QOpenGLShader::Fragment, _fragName);
-
-  if (_geoName.count())
-    program->addShaderFromSourceFile(QOpenGLShader::Geometry, _geoName);
-
+  enum {VERTEX, FRAGMENT, GEOMETRY};
+  using shdr = QOpenGLShader;
+  constexpr shdr::ShaderType qShaders[] = {shdr::Vertex, shdr::Fragment, shdr::Geometry};
+  for (auto shader : {VERTEX, FRAGMENT, GEOMETRY})
+  {
+    auto path = _shaderPaths[shader];
+    if (path == "") continue;
+    auto stdPath = path.toStdString();
+    if (!m_shaderParts.count(stdPath))
+    {
+      QOpenGLShader* shad = new QOpenGLShader(qShaders[shader]);
+      shad->compileSourceFile(path);
+      m_shaderParts[stdPath].reset(shad);
+    }
+    program->addShader(m_shaderParts[stdPath].get());
+  }
   program->link();
   m_shaderPrograms[_name].reset(program);
 }
