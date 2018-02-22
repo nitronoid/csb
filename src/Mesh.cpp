af61914af2ae94a86189efb6688748788c4da750
nitronoid <jackdiver@hotmail.co.uk>
2018-02-22 21:05:33 +0000

    Converted Mesh class to use an element buffer object to prevent vertex duplication, as this is more useful for my other projects.

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -16,15 +16,13 @@ void Mesh::load(const std::string &_fname, const size_t _meshNum)
         aiProcess_FlipUVs);
   const aiMesh* mesh = scene->mMeshes[_meshNum];
 
-  // Get the number of faces on the mesh
-  size_t numFaces = mesh->mNumFaces;
   // Calculate the amount of vertices we will store (3 per face)
-  size_t numStored = numFaces * 3;
+  size_t numVerts = mesh->mNumVertices;
 
   // Reserve memory in vectors to accomodate the incomming data
-  m_vertices.reserve(numStored * 3);
-  m_normals.reserve(numStored * 3);
-  m_uvs.reserve(numStored * 2);
+  m_vertices.reserve(numVerts);
+  m_normals.reserve(numVerts);
+  m_uvs.reserve(numVerts);
 
   // Get access to the information we will store
   auto& vertices = mesh->mVertices;
@@ -33,6 +31,22 @@ void Mesh::load(const std::string &_fname, const size_t _meshNum)
   // Some meshes don't have UV's
   bool hasTexCoords = mesh->HasTextureCoords(0);
 
+  for (size_t i = 0; i < numVerts; ++i)
+  {
+    auto& vert = vertices[i];
+    m_vertices.insert(m_vertices.end(), {vert.x,vert.y,vert.z});
+
+    auto& norm = normals[i];
+    m_normals.insert(m_normals.end(), {norm.x,norm.y,norm.z});
+
+    if (!hasTexCoords) continue;
+    // UV's only use the first two members
+    const auto& uv = hasTexCoords ? texCoords[i] : aiVector3D(0.0f, 0.0f, 0.0f);
+    m_uvs.insert(m_uvs.end(), {uv.x, uv.y});
+  }
+
+  // Get the number of faces on the mesh
+  size_t numFaces = mesh->mNumFaces;
   // We iterate through faces not vertices,
   // as the verts will be duplicated when used by more than one face.
   for (size_t faceIndex = 0; faceIndex < numFaces; ++faceIndex)
@@ -43,31 +57,22 @@ void Mesh::load(const std::string &_fname, const size_t _meshNum)
     {
       // Get the index of the vertex, we use this for it's normals and UV's too
       size_t vertInFace = face.mIndices[i];
-
-      // Get the vertex
-      auto& vert = vertices[vertInFace];
-      // Push it's values into our float vector
-      m_vertices.insert(m_vertices.end(), {vert.x,vert.y,vert.z});
-
-      // Do the same for normals
-      auto& normal = normals[vertInFace];
-      m_normals.insert(m_normals.end(), {normal.x,normal.y,normal.z});
-
-      // Lazy check for texCoords
-      if (!hasTexCoords) continue;
-
-      // UV's only use the first two members
-      const auto& uv = hasTexCoords ? texCoords[vertInFace] : aiVector3D(0.0f, 0.0f, 0.0f);
-      m_uvs.insert(m_uvs.end(), {uv.x, uv.y});
+      m_indices.push_back(vertInFace);
     }
   }
 }
 
 void Mesh::reset()
 {
+  m_indices.clear();
   m_vertices.clear();
   m_normals.clear();
   m_uvs.clear();
+}
+
+const GLushort *Mesh::getIndicesData() const noexcept
+{
+  return &m_indices[0];
 }
 
 const GLfloat* Mesh::getVertexData() const noexcept
@@ -97,6 +102,11 @@ const GLfloat *Mesh::getAttribData(const MeshAttributes::Attribute _attrib) cons
     default: break;
   }
   return data;
+}
+
+int Mesh::getNIndicesData() const noexcept
+{
+  return static_cast<int>(m_indices.size());
 }
 
 int Mesh::getNVertData() const noexcept
