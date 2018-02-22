02cf1e507af7382ea2d5937a49fdcf844844a579
nitronoid <jackdiver@hotmail.co.uk>
2018-02-22 19:19:39 +0000

    Added some distance constraints, need to switch to an indexed mesh format.

--- src/CSBmesh.cpp
+++ src/CSBmesh.cpp
@@ -1,4 +1,5 @@
 #include "CSBmesh.h"
+#include "gtx/fast_square_root.hpp"
 
 void CSBmesh::init()
 {
@@ -10,8 +11,21 @@ void CSBmesh::update(const float _time)
   constexpr auto gravity = glm::vec3(0.f,-0.1f,0.f);
   for (size_t i = 0; i < m_vertices.size(); ++i)
   {
-    glm::vec3 newPos = m_vertices[i] * 2.0f - m_prevPosition[i] + gravity * _time * _time;
-    m_prevPosition[i] = m_vertices[i];
-    m_vertices[i] = newPos;
+    auto& pos = m_vertices[i];
+    glm::vec3 newPos = pos * 2.0f - m_prevPosition[i] + gravity * _time * _time;
+    m_prevPosition[i] = pos;
+    pos = newPos;
+  }
+
+  constexpr auto restLength = 1.0f;
+  constexpr auto restSqr = restLength * restLength;
+  for (size_t i = 0; i < m_vertices.size() - 1; ++i)
+  {
+    auto& p1 = m_vertices[i];
+    auto& p2 = m_vertices[i + 1];
+    auto delta = p2 - p1;
+    delta *= restSqr / (delta * delta + restSqr) - 0.5f;
+    p1 -= delta;
+    p2 += delta;
   }
 }
