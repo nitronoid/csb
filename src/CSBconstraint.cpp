052b533e8600f2cffd8fb573572dcd4a1e085b0f
nitronoid <jackdiver@hotmail.co.uk>
2018-02-23 15:37:47 +0000

    Fixed distance constraint creation using an unordered edge pair set. This gives us the structural constraints. A basic test now runs involving a hard coded pin constraint.

--- src/CSBconstraint.cpp
+++ src/CSBconstraint.cpp
@@ -1,16 +1,20 @@
 #include "CSBconstraint.h"
+#include "gtx/fast_square_root.hpp"
 
 CSBconstraint::~CSBconstraint() = default;
 
 void DistanceConstraint::project(std::vector<glm::vec3> &_positions)
 {
-  auto restSqr = m_distance * m_distance;
   auto& p1 = _positions[m_p1];
   auto& p2 = _positions[m_p2];
   auto delta = p2 - p1;
-  delta *= restSqr / (delta * delta + restSqr) - 0.5f;
-  p1 -= delta;
-  p2 += delta;
+  auto deltaLen = glm::fastLength(delta);
+  auto diff = (deltaLen - m_distance) / deltaLen;
+  delta *= 0.5f * diff;
+
+  p1 += delta;
+  p2 -= delta;
+
 }
 
 
--- src/CSBmesh.cpp
+++ src/CSBmesh.cpp
@@ -1,14 +1,53 @@
 #include "CSBmesh.h"
 #include "gtx/fast_square_root.hpp"
+#include <tuple>
+#include <unordered_set>
+
+struct edgePair
+{
+  edgePair(const GLushort _a, const GLushort _b) :
+    p(std::min(_a, _b), std::max(_a, _b))
+  {}
+  friend bool operator==(const edgePair &_a, const edgePair &_b)
+  {
+    return _a.p == _b.p;
+  }
+  std::pair<GLushort, GLushort> p;
+};
+
+namespace std
+{
+template <>
+struct hash<edgePair>
+{
+  size_t operator()(const edgePair &_up) const
+  {
+    return std::hash<size_t>()(std::hash<GLushort>()(_up.p.first)) ^ std::hash<GLushort>()(_up.p.second);
+  }
+};
+}
 
 void CSBmesh::init()
 {
+  std::unordered_set<edgePair> edgeSet;
+  auto numEdges = m_vertices.size() + m_indices.size() / 3 - 2;
+  edgeSet.reserve(numEdges);
+
   m_prevPosition = m_vertices;
-  for (size_t i = 0; i < m_indices.size() - 1; ++i)
+  for (size_t i = 0; i < m_indices.size() - 2; i+=3)
   {
     auto p1 = m_indices[i];
     auto p2 = m_indices[i + 1];
-    if (p1 == p2) continue;
+    auto p3 = m_indices[i + 2];
+    edgeSet.insert({p1, p2});
+    edgeSet.insert({p2, p3});
+    edgeSet.insert({p3, p1});
+  }
+
+  for (const auto & edge : edgeSet)
+  {
+    auto p1 = edge.p.first;
+    auto p2 = edge.p.second;
     float distance = glm::fastDistance(m_vertices[p1], m_vertices[p2]);
     m_constraints.emplace_back(new DistanceConstraint(p1, p2, distance));
   }
@@ -16,7 +55,8 @@ void CSBmesh::init()
 
 void CSBmesh::update(const float _time)
 {
-  constexpr auto gravity = glm::vec3(0.f,-0.1f,0.f);
+  const auto pos = m_vertices[0];
+  const auto gravity = glm::vec3(0.f,-0.1f,0.f);
   for (size_t i = 0; i < m_vertices.size(); ++i)
   {
     auto& pos = m_vertices[i];
@@ -25,9 +65,11 @@ void CSBmesh::update(const float _time)
     pos = newPos;
   }
 
-  for (int i = 0; i < 3; ++i)
+  //for (int i = 0; i < 3; ++i)
   for (auto& constraint : m_constraints)
   {
     constraint->project(m_vertices);
   }
+
+  m_vertices[0] = pos;
 }
